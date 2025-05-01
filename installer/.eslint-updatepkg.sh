a544c993402577e24ee02f701d5dadfcb60a45de
Bjørn Jacobsen <bjornjac@pm.me>
2025-05-01 15:01:12 +0000

    Eslint 9+

--- installer/.eslint-updatepkg.sh
+++ installer/.eslint-updatepkg.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 
 INSTALL_PACKAGES=(
+	"eslint": "^9.25.1",
+	"@vue/eslint-config-prettier": "^10.2.0",
+	"@vue/eslint-config-typescript": "^14.5.0",
 	"@typescript-eslint/eslint-plugin@^8.30.1"
 	"@typescript-eslint/parser@^8.30.1"
-	"eslint@^8.57.1"
-	"eslint-config-standard@^17.1.0"
-	"eslint-plugin-import@^2.31.0"
-	"eslint-plugin-vue@^10.0.0"
+	"eslint-plugin-import": "^2.31.0",
+	"eslint-plugin-vue": "^10.0.0",
 	"prettier@^3.5.3"
 );
 
 REMOVE_PACKAGES=(
+	"eslint"
 	"tslint"
-	"standard"
-	"xo"
 );
 
 if command -v pnpm &> /dev/null; then
