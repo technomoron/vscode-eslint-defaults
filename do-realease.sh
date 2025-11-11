d76019546c3b8f668eb0f09607cf4565a6b85237
BEJ <bjornjac@pm.me>
2025-11-11 08:22:34 +0000

    chore: prune legacy installer configs

--- do-realease.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-VERSION=$(node -p "require('./package.json').version")
-
-echo "Creating release for ${VERSION}"
-
-git tag -a "v${VERSION}" -m "Release version ${VERSION}"
-git push origin "v${VERSION}"
-
